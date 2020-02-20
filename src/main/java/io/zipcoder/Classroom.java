9d77c5abac2836a66d0d8e4f2eb47fe0635c4c35
dbond <debon1426@gmail.com>
2020-02-20 17:03:14 -0500

    made a classroom of students

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,117 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Classroom {
+
+    ArrayList<Student> studentsInClass = new ArrayList<>();
+    int maxNumberOfStudents;
+    Student students;
+    public Classroom(int maxNumberOfStudents) {
+        this.maxNumberOfStudents = maxNumberOfStudents;
+    }
+
+    public Classroom(int maxNumberOfStudents, Student... students) {
+        this.maxNumberOfStudents = maxNumberOfStudents;
+        for (Student student :students){
+            studentsInClass.add(student);
+        }
+    }
+    public Classroom(Student... students) {
+        for (Student student : students) {
+            this.studentsInClass.add(student);
+        }
+    }
+
+
+
+    public ArrayList<Student>  getStudents() {
+
+        return studentsInClass;
+    }
+
+    public void setStudent(Student student) {
+        this.students = student;
+    }
+
+
+    public void removeStudent ( Student student){
+        studentsInClass.remove(student);
+    }
+
+
+
+
+
+    public int getMaxNumberOfStudents() {
+        return maxNumberOfStudents;
+    }
+
+    public void setMaxNumberOfStudents(int maxNumberOfStudents) {
+        this.maxNumberOfStudents = maxNumberOfStudents;
+    }
+
+    public double getAverageExamScore() {
+        double  total = 0;
+        for ( Student s : studentsInClass){
+            total += s.getNumberOfExamsTaken();
+        }
+        return total/studentsInClass.size();
+ }
+
+    public void add(Student student){
+        studentsInClass.add(student);
+    }
+
+
+    public Student getStudentsByScore (Double score){
+
+        for (Student student :studentsInClass){
+            ArrayList<Double> testScores = student.testScores;
+            for (int i = 0; i < testScores.size(); i++) {
+                Double exam = testScores.get(i);
+                if (exam.equals(score)) {
+                    return student;
+                }
+
+            }
+        }
+      return students;
+
+    }
+
+
+
+    public ArrayList<String>  getGradeBook () {
+        Double classAverage = getAverageExamScore();
+        ArrayList<String> grades = new ArrayList<>();
+
+        for ( Student student : studentsInClass){
+                 Double average= student.getAverageExamScore();
+                  if ((average>=90.0)){
+                      student.setGrade("A");
+                      grades.add(student.getGrade());
+            }if (( average<=100) && (average>=90)){
+                      student.setGrade("B");
+                grades.add(student.getGrade());
+            }if (( average<=89.0) && (average>=80.0)){
+                student.setGrade("C");
+                grades.add(student.getGrade());
+            }if (( average<=79.0) && (average>=70.0)){
+                student.setGrade( "D");
+                grades.add(student.getGrade());
+            }if (( average<=69.0) && (average>=60.0)){
+                student.setGrade( "E");
+                grades.add(student.getGrade());
+            }if (( average<=59.0) && (average>=0.0)){
+               student.setGrade( "F");
+                grades.add(student.getGrade());
+            }
+
+        }
+
+    return grades;
+    }
+
+
 }
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,117 @@
 package io.zipcoder;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
 public class Student {
+    String firstName;
+    String lastName;
+    String grade;
+    ArrayList<Double> testScores = new ArrayList<>();
+
+    public Student(String firstName, String lastName, Double[] examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        for (Double scores : examScores) {
+            testScores.add(scores);
+        }
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public void setTestScores(ArrayList<Double> testScores)
+    {
+        this.testScores = testScores;
+    }
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public ArrayList<Double> getTestScores() {
+        return testScores;
+    }
+    public double getNumberOfExamsTaken() {
+        int total = 0;
+        for (Double score : testScores) {
+            total += score;
+
+        }
+        return total;
+    }
+
+    public String getExamScores() {
+        String exams = "";
+        int count = 1;
+        for (int i = 0; i < testScores.size(); i++) {
+            DecimalFormat format = new DecimalFormat("0.#");
+            exams += "Exam " + count++ + " -> " + format.format(testScores.get(i)) + "\n";
+
+        }
+
+
+
+
+        return exams;
+    }
+
+    public void addExamScore(Double score) {
+
+        testScores.add(score);
+    }
+
+    public int setExamScore(int examNumber, double newScore) {
+        double value = 0;
+        for (int i = 0; i < testScores.size(); i++) {
+            value = testScores.set(i, newScore);
+        }
+        int integer = (int) value;
+        return integer;
+
+    }
+
+    public double getAverageExamScore() {
+
+
+        return getNumberOfExamsTaken() / testScores.size();
+    }
+
+    public String toString() {
+        DecimalFormat format = new DecimalFormat("0.#");
+
+        String studentName = getFirstName() + " " + getLastName();
+        String averageScore = String.valueOf(format.format(getAverageExamScore()));
+        String result = "";
+
+
+        result = "Student Name: " + studentName + "\n" + "Average Score: " +
+                averageScore + "\n" + "Exam Scores: \n" + getExamScores();
+        return result;
+    }
+
+    public String toString( String firstName){
+
+        String  studentName = getFirstName() + " " + getLastName();
+        return studentName;
+
+  }
 }
